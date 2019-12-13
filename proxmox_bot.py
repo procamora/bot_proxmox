4da4946a835a2a9280dd1e21ca59e56f31fc3a84
procamora <pablojoserocamora@gmail.com>
2019-12-13 20:08:15 +0100

    update bot

--- proxmox_bot.py
+++ proxmox_bot.py
@@ -9,7 +9,7 @@
 wakeonlan - Start the server
 reboot - Restart the server
 poweroff - Turn off the server
-halt - Turn off the server
+shutdown - Turn off the server
 exit - Exit
 help - Show help
 start - Start the bot
@@ -27,7 +27,11 @@ from telebot import TeleBot, types  # Importamos la librería Y los tipos especi
 from Pentesting import client_ssh
 from logger import logger
 
-FILE_CONFIG = 'settings.ini'
+FILE_CONFIG: Path = Path('settings.ini')
+if not FILE_CONFIG.exists():
+    logger.critical(f'File {FILE_CONFIG} not exists and is necesary')
+    sys.exit(1)
+
 config: configparser.ConfigParser = configparser.ConfigParser()
 config.read(FILE_CONFIG)
 
@@ -48,12 +52,13 @@ if not cert.exists():
 
 bot: TeleBot = TeleBot(config_basic.get('BOT_TOKEN'))
 bot.send_message(administrador, "El bot se ha iniciado")
+logger.info('Starting bot')
 
 dicc_botones: Dict = {
     'wakeonlan': '/wakeonlan',
     'reboot': '/reboot',
     'poweroff': '/poweroff',
-    'halt': '/halt',
+    'shutdown': '/shutdown',
     'exit': '/exit',
 }
 
@@ -79,6 +84,14 @@ def execute_command(command: str) -> Tuple[str, str, subprocess.Popen]:
     return format_text(stdout), format_text(stderr), execute
 
 
+def get_keyboard() -> types.ReplyKeyboardMarkup:
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.row(dicc_botones['wakeonlan'], dicc_botones['reboot'])
+    markup.row(dicc_botones['poweroff'], dicc_botones['shutdown'])
+    markup.row(dicc_botones['exit'])
+    return markup
+
+
 # Handle always first "/start" message when new chat with your bot is created
 @bot.message_handler(commands=["start"])
 def command_start(message) -> NoReturn:
@@ -97,34 +110,11 @@ def command_help(message) -> NoReturn:
     return  # solo esta puesto para que no falle la inspeccion de codigo
 
 
-@bot.message_handler(func=lambda message: message.chat.id == administrador, content_types=["text"])
-def my_text(message) -> NoReturn:
-    if message.text in dicc_botones.values():
-        if message.text == dicc_botones['wakeonlan']:
-            send_wakeonlan(message)
-        elif message.text == dicc_botones['reboot']:
-            send_reboot(message)
-        elif message.text == dicc_botones['poweroff']:
-            send_poweroff(message)
-        elif message.text == dicc_botones['halt']:
-            send_halt(message)
-        elif message.text == dicc_botones['exit']:
-            send_exit(message)
-    else:
-        bot.send_message(message.chat.id, "Comando desconocido")
-    return  # solo esta puesto para que no falle la inspeccion de codigo
-
-
 @bot.message_handler(commands=["system"])
 def command_system(message) -> NoReturn:
     bot.send_message(message.chat.id, "Lista de comandos disponibles")
 
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.row(dicc_botones['wakeonlan'], dicc_botones['reboot'])
-    markup.row(dicc_botones['poweroff'], dicc_botones['halt'])
-    markup.row(dicc_botones['exit'])
-
-    bot.send_message(message.chat.id, "Escoge una opcion: ", reply_markup=markup)
+    bot.send_message(message.chat.id, "Escoge una opcion: ", reply_markup=get_keyboard())
     return  # solo esta puesto para que no falle la inspeccion de codigo
 
 
@@ -133,7 +123,7 @@ def send_exit(message) -> NoReturn:
     pass
 
 
-@bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['wakeonlan'])
+@bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['/wakeonlan'])
 def send_wakeonlan(message) -> NoReturn:
     command = f'wakeonlan {config_basic.get("MAC_PROXMOX")}'
     stdout, stderr, execute = execute_command(command)
@@ -142,9 +132,9 @@ def send_wakeonlan(message) -> NoReturn:
         bot.reply_to(message, f'Error: {stderr}')
         return
     elif len(stdout) == 0:
-        bot.reply_to(message, 'Ejecutado, no hace nada')
+        bot.reply_to(message, 'Ejecutado, no hace nada', reply_markup=get_keyboard())
     else:
-        bot.reply_to(message, stdout)
+        bot.reply_to(message, stdout, reply_markup=get_keyboard())
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['reboot'])
@@ -158,9 +148,9 @@ def send_reboot(message) -> NoReturn:
     output, error = ssh.execute_command(user=config_ssh.get('USER'), password=config_ssh.get('PASSWORD'), cert=cert,
                                         command=cmd)
     if error != 0:
-        bot.reply_to(message, f'Error: {output}')
+        bot.reply_to(message, f'Error: {output}', reply_markup=get_keyboard())
     else:
-        bot.reply_to(message, f'OK: {output}')
+        bot.reply_to(message, f'rebooting in process', reply_markup=get_keyboard())
     return
 
 
@@ -176,15 +166,15 @@ def send_poweroff(message) -> NoReturn:
     output, error = ssh.execute_command(user=config_ssh.get('USER'), password=config_ssh.get('PASSWORD'), cert=cert,
                                         command=cmd)
     if error != 0:
-        bot.reply_to(message, f'Error: {output}')
+        bot.reply_to(message, f'Error: {output}', reply_markup=get_keyboard())
     else:
-        bot.reply_to(message, f'OK: {output}')
+        bot.reply_to(message, f'OK: {output}', reply_markup=get_keyboard())
     return
 
 
-@bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['halt'])
-def send_halt(message) -> NoReturn:
-    cmd: str = 'halt'
+@bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['shutdown'])
+def send_shutdown(message) -> NoReturn:
+    cmd: str = 'shutdown -h now'
     ssh: client_ssh.ClientSSH = client_ssh.ClientSSH(ip=config_ssh.get('IP'), port=config_ssh.get('PORT'), debug=False)
 
     if not ssh.is_online():
@@ -194,9 +184,9 @@ def send_halt(message) -> NoReturn:
     output, error = ssh.execute_command(user=config_ssh.get('USER'), password=config_ssh.get('PASSWORD'), cert=cert,
                                         command=cmd)
     if error != 0:
-        bot.reply_to(message, f'Error: {output}')
+        bot.reply_to(message, f'Error: {output}', reply_markup=get_keyboard())
     else:
-        bot.reply_to(message, f'OK: {output}')
+        bot.reply_to(message, f'OK: {output}', reply_markup=get_keyboard())
     return
 
 
@@ -209,14 +199,14 @@ def handle_cmd(message) -> NoReturn:
         stdout, stderr, execute = execute_command(command)
 
         if check_error(execute, stderr):
-            bot.reply_to(message, f'Error: {stderr}')
+            bot.reply_to(message, f'Error: {stderr}', reply_markup=get_keyboard())
             return
         elif len(stdout) == 0:
-            bot.reply_to(message, f'Ejecutado: {command}')
+            bot.reply_to(message, f'Ejecutado: {command}', reply_markup=get_keyboard())
         else:
-            bot.reply_to(message, stdout)
+            bot.reply_to(message, stdout, reply_markup=get_keyboard())
     else:
-        bot.reply_to(message, 'Comando aun no implementado')
+        bot.reply_to(message, 'Comando aun no implementado', reply_markup=get_keyboard())
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, content_types=["photo"])
@@ -246,11 +236,20 @@ def my_document(message) -> NoReturn:
     return  # solo esta puesto para que no falle la inspeccion de codigo
 
 
+@bot.message_handler(func=lambda message: message.chat.id == administrador, content_types=["text"])
+def my_text(message) -> NoReturn:
+    if message.text in dicc_botones.values():
+        pass
+    else:
+        bot.send_message(message.chat.id, "Comando desconocido", reply_markup=get_keyboard())
+    return  # solo esta puesto para que no falle la inspeccion de codigo
+
+
 @bot.message_handler(regexp=".*")
 def handle_resto(message) -> NoReturn:
     texto = 'No tienes permiso para ejecutar esta accion, eso se debe a que no eres yo.\n' \
             'Por lo que ya sabes, desaparece -.-'
-    bot.reply_to(message.chat.id, texto)
+    bot.reply_to(message, texto)
     return  # solo esta puesto para que no falle la inspeccion de codigo
 
 
